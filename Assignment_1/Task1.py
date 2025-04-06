81c7e99188093c1493d0654618be0a003ce9a26c
vinaybhagat3 <78097136+vinaybhagat3@users.noreply.github.com>
2025-04-06 20:05:46 +0530

    Add files via upload

---
+++ Assignment_1/Task1.py
@@ -0,0 +1,8 @@
+#Taking input from user
+a = int(input('Enter the first number : '))
+b = int(input('Enter the Second number : '))
+#Performing simple mathematical calculations and printing output.
+print('Addition : ',a+b)
+print('Subtraction : ',a-b)
+print('Multiplication : ',a*b)
+print('Division : ',a/b)
---
+++ Assignment_1/Task2.py
@@ -0,0 +1,6 @@
+#Taking input from user
+a = input("Enter your first name : ")
+b = input("Enter your last name : ")
+
+#Printing both the strings using concatenation.
+print ("Hello,",a + b,"! Welcome to Python Program")
